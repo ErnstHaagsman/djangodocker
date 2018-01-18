fc175369785441bb1835919f8f3b4f8fab3b3ee8
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-01-18 14:39:34 +0100

    Upgrade to Django 2.0.1

--- djangodocker/models.py
+++ djangodocker/models.py
@@ -5,4 +5,5 @@ class Todo(models.Model):
     todo_text = models.CharField(max_length=400)
     created_at = models.DateTimeField(auto_now=True)
     done = models.BooleanField(default=False)
-    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE)
