e18e5b8e3560c59752acb9c8071a52a7facd0b05
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2018-01-24 16:29:08 +0100

    Specify logging configuration

--- djangodocker/settings.py
+++ djangodocker/settings.py
@@ -96,6 +96,22 @@ DATABASES = {
     }
 }
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'INFO'),
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
